b9396bfb452608cdccbf1bbc87e725a0ea075f47
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-05-12 10:14:12 -0300

    Update Desafio 039.py
    Feito alterações conforme a resolução do desafio 39

--- Desafio 039.py
+++ Desafio 039.py
@@ -1,10 +1,18 @@
 # Faça um programa que leia o ano de nascimento de um jovem e mostre de acordo com a sua idade
 # se ele ainda vai se alistar no exercito, se é hora de se alistar ou se passou o tempo de alistamento.
-# o programa deve mostar o tempo que falta ou o tempo que passou.
-idade = int(input('Digite a sua idade: '))
+from datetime import date
+ano_nascimento = int(input('Digite o ano do seu nascimento: '))
+ano_atual = date.today()
+ano_atual = ano_atual.year
+idade = ano_atual - ano_nascimento
 if idade == 18:
-    print('Você deve se alistar esse ano!')
+    print('Você tem 18 anos e deve se alistar IMEDIATAMENTE!')
+elif idade > 18:
+    print('Você tem {} anos, o seu tempo de se alistar já passou!'.format(idade))
+    ano = ano_nascimento + 18
+    print('Você deveria se alistar em {}'.format(ano))
 elif idade < 18:
-    print('Faltam {} anos para você se alistar!'.format(18 - idade))
-else:
-    print('Já se passaram {} anos do seu alistamento.'.format(idade - 18))
+    print('Você tem {} anos'.format(idade))
+    print('Você aida é menor de idade!')
+    ano = ano_nascimento + 18
+    print('Você vai se alistar em {}'.format(ano))
