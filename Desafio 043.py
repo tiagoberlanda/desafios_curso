20f18e81e604effbdf12c664ed0c2e252cdcd298
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-07-09 13:33:10 -0300

    Update Desafio 043.py
    Feito o desfio

--- Desafio 043.py
+++ Desafio 043.py
@@ -2,3 +2,18 @@
 # de acordo com as seguintes condições
 # abaixo de 18.5: abaixo do peso, entre 18.5 e 25 :Peso Ideal, 25 até 30: sobrepeso,30 até 40:obesidade
 # acima de 40:obesidade mórbida
+altura = float(input('Digite a sua altura: '))
+peso = float(input('Digite o seu peso: '))
+imc = (peso / (altura * altura))
+print('O Seu IMC vale {:.2f}'.format(imc))
+if imc < 18.5:
+    condicao = 'Abaixo do Peso'
+elif imc >= 18.5 and imc < 25:
+    condicao = 'Peso Ideal'
+elif imc >= 25 and imc < 30:
+    condicao = 'Sobrepeso'
+elif imc >= 30 and imc < 40:
+    condicao = 'Obesidade'
+elif imc >= 40:
+    condicao = 'Obesidade Morbida'
+print('Diante disso você está classificado como {}'.format(condicao))
