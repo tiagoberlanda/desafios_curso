b14bd5a6971feb78c01bc3d4ed70f86d84912d7d
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-11-19 14:12:59 -0300

    Update Desafio 042.py
    Removido o tipo do triângulo quando não poder formar triangulo.

--- Desafio 042.py
+++ Desafio 042.py
@@ -15,6 +15,7 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
 else:
     print('Os Segmentos acima {}NÃO PODEM{} formar um triângulo!'.format
     (cor['vermelho'], cor['limpa']))
+    exit()
     
 if r1 == r2 and r1 == r3:
     tipo = 'Equilátero'
