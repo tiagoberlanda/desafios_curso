b06ea4b3c6afe8e86426e2b370238ebe4c890b6f
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-11-22 15:24:28 -0300

    Update Desafio 051.py

--- Desafio 051.py
+++ Desafio 051.py
@@ -1,7 +1,11 @@
 # Crie um programa que leia o primeiro termo e a razão de uma PA.
 # No final mostre os 10 primeiros termos dessa progressão
+
 termo = int(input('Digite o termo da PA: '))
 progressao = int(input('Digite a progressão da PA: '))
+decimo = termo + (10 - 1) * progressao
 print("Os primeiros termos dessa progressão são:")
-for c in range(termo, 11, progressao):
-    print(c)
+for c in range(termo, decimo + progressao, progressao):
+    print(c, end=' -> ')
+
+print('Acabou')
