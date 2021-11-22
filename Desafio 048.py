2b8188f7c62d0efa6278c716cbc6d9e8b1199a1f
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-11-22 11:44:36 -0300

    Update Desafio 048.py

--- Desafio 048.py
+++ Desafio 048.py
@@ -4,6 +4,6 @@ print('''A soma de todos os números impares
 dívisiveis por 3 no intervalor de 1 e 500 são:''', end = " ")
 soma = 0
 for c in range(1, 500):
-    if c % 3 == 0:
+    if c % 3 == 0 and c % 2 != 0:
         soma = soma + c
 print(soma)
