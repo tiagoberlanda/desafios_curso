464ace4450be8646e1aa1209f7f23679cb0ab7cf
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-11-22 14:27:29 -0300

    Update Desafio 049.py

--- Desafio 049.py
+++ Desafio 049.py
@@ -1,3 +1,5 @@
 # Refaça o desafio 009 mostrando a tabuada porem utilizando o for
-
 numero = int(input('Digite um número para saber sua tabuada: '))
+for c in range(1,11):
+    print('{} x {} = {}'.format(numero, c,numero*c))
+print('Fim')
