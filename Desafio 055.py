5abb5ff67c6df7422b6b95938a7ef895b012073d
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-12-30 15:16:24 -0300

    Update Desafio 055.py

--- Desafio 055.py
+++ Desafio 055.py
@@ -1,2 +1,15 @@
 # Crie um programa que leia o peso de 5 pessoas
 # depois mostre quem tem o maior e o menor peso lidos.
+maior = 0
+menor = 0
+for p in range(1,6):
+    peso = float(input('Peso da {}° pessoa: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso foi de {}KG e o menor foi de {}KG'.format(maior, menor))
