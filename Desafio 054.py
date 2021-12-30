6247c10e20e495e964db533ba9008ff93816c105
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-12-30 15:01:07 -0300

    Update Desafio 054.py

--- Desafio 054.py
+++ Desafio 054.py
@@ -3,15 +3,13 @@
 from datetime import date
 data = date.today()
 ano = data.year
-
 menor = 0
 maior = 0
 for c in range(0,7):
     pessoa = int(input('Digite o ano de nascimento da pessoa:'))
     idade = ano - pessoa
     if idade < 18:
-        menor = menor + 1
+        menor += 1
     else:
-        maior = maior + 1
+        maior += 1
 print('Tem {} pessoas maiores de idade e {} menores de idade'.format(maior, menor))
-
